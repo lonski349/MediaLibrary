a08ef2b70eeab5c3354c4d4c0e831bb436b024a5
Brian Harman <harman.brian.s@gmail.com>
2016-02-14 14:30:43 -0700

    “message”

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -78,7 +78,7 @@ int main() {
     //      << "Wreck It Ralph has been removed from the library. \n";
 
     //Instantiate Json MediaLibrary
-    MediaLibrar::jLibrary = MediaLibrary("media.json");
+    MediaLibrary::jLibrary = MediaLibrary("media.json");
 
     //Displays the updated list of items in the library
     cout << "==================================================\n";
